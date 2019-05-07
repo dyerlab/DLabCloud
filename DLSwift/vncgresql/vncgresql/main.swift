889cfa8b734e44955ce1a87c90728ab14127abc4
dyerlab <rjdyer@vcu.edu>
2019-05-07 07:24:03 -0400

    adding swift section

---
+++ DLSwift/vncgresql/vncgresql/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  vncgresql
+//
+//  Created by Rodney Dyer on 5/7/19.
+//  Copyright © 2019 Rodney Dyer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
